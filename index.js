b0ec3204d406b193eddba5dfe9f7df92353a5fe7
mainy1995 <yannik.meinke@web.de>
2019-04-09 14:41:25 +0200

    fixed konstis commands

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const { Composer, log, session } = require('micro-bot')
 var mysql = require('mysql');
 
 //Read SQL Connection from console
-const mysql_password =  process.env.MYSQL_PASSWORD
+const mysql_password = process.env.MYSQL_PASSWORD
 if (!mysql_password) {
-  console.error(`μ-bot: Please supply MYSQL_PASSWORD`)
-  process.exit(1)
+    console.error(`μ-bot: Please supply MYSQL_PASSWORD`)
+    process.exit(1)
 }
-const mysql_host =  process.env.MYSQL_HOST
+const mysql_host = process.env.MYSQL_HOST
 if (!mysql_host) {
-  console.error(`μ-bot: Please supply MYSQL_HOST`)
-  process.exit(1)
+    console.error(`μ-bot: Please supply MYSQL_HOST`)
+    process.exit(1)
 }
 
 const mysql_user = process.env.MYSQL_USER
@@ -24,14 +24,14 @@ if (!mysql_user) {
     console.error(`μ-bot: Please supply MYSQL_DB`)
     process.exit(1)
 }
-  
+
 //Create SQL Connection
 var con = mysql.createConnection({
     host: mysql_host,
     user: mysql_user,
     password: mysql_password,
     database: mysql_db
-    });
+});
 
 const group_id = process.env.GROUP_ID
 if (!mysql_user) {
@@ -69,7 +69,7 @@ bot.on('animation', (ctx) => handle_media_message(ctx, animation_id, send_animat
  */
 function photo_id(message) {
     if (!has_photo(message)) return null
-    return message.photo[message.photo.length-1].file_id
+    return message.photo[message.photo.length - 1].file_id
 }
 
 /**
@@ -180,12 +180,6 @@ function handle_media_message(ctx, file_id_callback, send_media) {
     conOpen(con, function () {
         //insert photo and publisher in database
         var file_id = file_id_callback(ctx.message)
-        var user_id = ctx.message.from.id
-        var sql = "INSERT INTO memes (UserID, photoID) VALUES ( '" + user_id + "','" + file_id + "')";
-        con.query(sql, function (err, result) {
-            if (err&&err.sqlMessage.includes('photoID')) {
-                ctx.telegram.sendMessage(user_id,'REPOST DU SPAST!'); 
-            }else if(err){
         var user = ctx.message.from
         if (!ctx.message.from.is_bot) {
             save_user(ctx.message.from);
@@ -198,22 +192,14 @@ function handle_media_message(ctx, file_id_callback, send_media) {
                 console.log("=========================SQL QUERY===========================")
                 console.log(sql)
                 console.log(err);
-            } 
+            }
             //send the meme to Memehub with inlinekeyboard
             send_media(
                 ctx,
                 group_id,
-                file_id, 
-                { 
-                    caption: "@" + ctx.message.from.username, 
-                    reply_markup: { 
-                        inline_keyboard: [[{ text: "👍", callback_data: "upvote" }]] 
-                    } 
-                }
-            ); //, { text: "👎", callback_data: "downvote" } 
                 file_id,
                 {
-                    caption: "@" + ctx.message.from.username+" | #"+ctx.message.caption,
+                    caption: "@" + ctx.message.from.username + " | #" + ctx.message.caption,
                     reply_markup: {
                         inline_keyboard: [[{ text: "👍", callback_data: "upvote" }]]
                     }
@@ -233,7 +219,6 @@ function handle_media_message(ctx, file_id_callback, send_media) {
                     })
                 });
             });
-
         });
     })
 }
@@ -242,18 +227,10 @@ function handle_media_message(ctx, file_id_callback, send_media) {
 
 
 //do this on an callback query
+//do this on an callback query
 bot.on('callback_query', (ctx) => {
     let file_id = any_media_id(ctx.update.callback_query.message)
     let upvotes;
-    let user=ctx.update.callback_query.from.id;
-    console.log(ctx.update)
-    switch (ctx.update.callback_query.data) {
-        case "upvote":
-            con.connect(function (err) {
-                if (err) console.log(err);
-                var sql = "INSERT INTO votes (userID, photoID, vote) VALUES ('" + user + "','" + file_id + "', true) ON DUPLICATE KEY UPDATE vote = !vote;";
-                console.log(sql);
-
     let user = ctx.update.callback_query.from;
     console.log("=======================UPVOTE CALLBACK QUERY=======================")
     console.log(ctx.update)
@@ -264,7 +241,6 @@ bot.on('callback_query', (ctx) => {
         case "upvote":
             conOpen(con, function () {
                 var sql = "INSERT INTO votes (userID, photoID, vote) VALUES ('" + user.id + "','" + file_id + "', true) ON DUPLICATE KEY UPDATE vote = !vote;";
-
                 con.query(sql, function (err, result) {
                     if (err) {
                         console.log("=========================SQL QUERY===========================")
@@ -274,11 +250,6 @@ bot.on('callback_query', (ctx) => {
                 });
                 var sql = "select sum(vote) as upvotes  from votes where photoID='" + file_id + "';";
                 con.query(sql, function (err, result) {
-
-                    if (err) console.log(err);
-                    console.log(result[0].upvotes);
-                    upvotes=result[0].upvotes;
-                    ctx.editMessageReplyMarkup({inline_keyboard: [[{ text: "👍 - " + upvotes, callback_data: "upvote" }]]});
                     if (err) {
                         console.log("=========================SQL QUERY===========================")
                         console.log(sql)
@@ -288,109 +259,74 @@ bot.on('callback_query', (ctx) => {
                     ctx.editMessageReplyMarkup({ inline_keyboard: [[{ text: "👍 - " + upvotes, callback_data: "upvote" }]] });
                     ctx.answerCbQuery();
                 });
-            });           
-            break;       
-        default:        
+            });
+            break;
+        default:
             ctx.answerCbQuery();
             break;
     }
 
 })
 
-module.exports = bot
-
-// zeigt das Meme mit den meisten Upvotes an 
+// zeigt mein Meme mit den meisten Upvotes an 
 bot.command('stats', (ctx) => {
-    let user_id = ctx.message.from.id;
-    let photo;
-    let chatId;
+    let user_id = ctx.message.from.id;  
+    let photo, max_upvotes
+    let chatId = ctx.message.chat.id;
     let extra;
-    let max_upvotes;
-    
-    //ctx.reply(user_id);
-    //var sql = "SELECT photoID as photo, vote from test.votes where userID = '"+ user_id + "' order by vote desc;";// sql Auruf bei dem für den Benutzer das Bild mit den meisten Upvotes geholt wird
-    var sql = "Create or Replace view Statistik as select m.photoID as meme, count(v.vote) as upvotes from test_statistik.memes m Inner join test_statistik.votes v on (m.photoID = v.photoID) where m.userID= '"+ user_id + "' group by m.photoID;"; 
-    con.query(sql, function (err, result) {
-        if (err) console.log(err);
-        console.log(sql)
-
-        })
-        var sql_ = "Select meme as meme, max(upvotes) as upvotes from Statistik;";
-    
-    con.query(sql_, function (err, result) {
-            if (err) console.log(err);
-            
-            console.log(result[0].meme);
-            console.log(result[0].upvotes);
-            console.log(sql_)
-            photo=result[0].meme
-            max_upvotes=result[0].upvotes
-            console.log(photo)
-            
-            
-            chatId=ctx.message.chat.id;
-            
+    conOpen(con, function () {        
+        var sql = "Select photoID, max(upvotes) as upvotes from Statistik where UserID='"+user_id +"';";    
+        con.query(sql, function (err, result) {
+            if (err) console.log(err);                
+            photo = result[0].meme
+            max_upvotes = result[0].upvotes
             console.log(chatId);
-            send_photo(ctx,chatId,photo,extra);
-            ctx.telegram.sendMessage(chatId,'Wir präsentieren: Dein Meme mit den meisten Upvotes -> ' + max_upvotes);
+            send_photo(ctx, chatId, photo,extra);
+            ctx.telegram.sendMessage(chatId, 'Wir präsentieren: Dein Meme mit den meisten Upvotes -> ' + max_upvotes);
             //sendPhoto(chatId, photo)
-           
-    })
-})
-
-// zeigt durchschnittlichen Upvotes an 
-bot.command('avg', (ctx) => {
-    let user_id = ctx.message.from.id;
-    let photo;
-    let chatId;
-    let extra;
-    let avg;
-
-    var sql = "Create or Replace view Statistik as select m.photoID as meme, count(v.vote) as upvotes from test_statistik.memes m Inner join test_statistik.votes v on (m.photoID = v.photoID) where m.userID= '"+ user_id + "' group by m.photoID;"; 
-    con.query(sql, function (err, result) {
-        if (err) console.log(err);
-        console.log(sql)
+    
         })
-
-    var sql_ = "Select round(avg(upvotes),2) as average from Statistik;";
-    con.query(sql_, function (err, result) {
-        avg = result[0].average;
-        chatId=ctx.message.chat.id;
-            
-            console.log(chatId);
-            ctx.telegram.sendMessage(chatId,'Dein Upvote-Average (all-time): ' + avg);
     })
+   
 })
 
+// zeigt durchschnittliche Upvotes auf meine Memes an 
+bot.command('avg', (ctx) => {
+    let user_id = ctx.message.from.id;    
+    let chatId= ctx.message.chat.id;    
+    let avg;
+    conOpen(con, function(){       
+        var sql_ = "Select round(avg(upvotes),2) as average from Statistik where UserID='"+user_id +"';";
+        con.query(sql_, function (err, result) {
+            avg = result[0].average;
+            ctx.telegram.sendMessage(chatId, 'Dein Upvote-Average (all-time): ' + avg);
+        })
+    })
+   
+})
 // zeigt memer mit deren Anzahl an Uploads an ( aber noch ohne Name nur mit userID)
 bot.command('meme_sum', (ctx) => {
-    let user_id = ctx.message.from.id;
-    let chatId;
-    let extra;
-    let id;
-   
-    chatId=ctx.message.chat.id;
-    let sum;
+    let chatId= ctx.message.chat.id;
+    let sum='';
+    conOpen(con, function(){
+        var sql = "Select *  from MemeAnzahl order by meme_sum desc;";
+        con.query(sql, function (err, result) {
+            if (err) console.log(err);
+                
+            for (i = 0; i < result.length; i++) {
+                sum+='Platz ' + (i + 1) + ': ' + result[i].meme_sum + (result[i].Username!=null? '       Memer: @' + result[i].Username:'       UserID: ' + result[i].UserID)+'\n'
+            }
+            ctx.telegram.sendMessage(chatId, sum);
     
-    var sql = "Select userID, count(photoID) as meme_sum from memes group by userID order by meme_sum desc;"; 
-    con.query(sql, function (err, result) {
-        if (err) console.log(err);
-        console.log(sql)
-        
-        for (i=0;i<result.length;i++){
-            sum = result[i].meme_sum;
-            id = result[i].userID;
-            console.log(sum);
-            ctx.telegram.sendMessage(chatId,'Platz ' + (i+1) +': ' + sum +'       Memer: @'+ id);
-        }
+    
+    
+        })
+    })
+    
 
 
-       
-    })    
-           
-            
 })
 
-    
-    
-   
+
+
+module.exports = bot
