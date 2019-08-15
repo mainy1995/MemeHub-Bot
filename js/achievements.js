632dd46fc37938c329cad38ab3596e84a8dbc3c3
LeifB <leif.bernsdorf@gmail.com>
2019-08-16 00:42:06 +0200

    Prevent vote achievement spamming

--- js/achievements.js
+++ js/achievements.js
@@ -1,6 +1,8 @@
 const db = require('./mongo-db');
 const config = require('../config.json');
 
+let recent_vote_achievements = {};
+
 async function check_post_archievements(ctx) {
     try {
         const result = await db.get_user_meme_count(ctx.message.from.id);
@@ -18,15 +20,19 @@ async function check_post_archievements(ctx) {
 async function check_vote_achievements(ctx, file_id, vote_type) {
     try {
         const poster_id = await db.get_user_from_meme(file_id);
-        const upvotes = await db.count_user_total_votes_by_type(poster_id, vote_type);
+        const votes = await db.count_user_total_votes_by_type(poster_id, vote_type);
         if (!config.achievements.vote_count[vote_type]) return;
 
-        const achievement = config.achievements.vote_count[vote_type].find(a => a.count === upvotes);
+        const achievement = config.achievements.vote_count[vote_type].find(a => a.count === votes);
 
         if (!achievement) return;
 
         const poster = await db.get_user(poster_id);
+
+        if (recent_vote_achievements[`${poster._id}:${vote_type}`] == votes) return;
+
         ctx.telegram.sendMessage(config.group_id, fromatMessage(achievement, poster));
+        recent_vote_achievements[`${poster._id}:${vote_type}`] = votes;
     }
     catch (err) {
         console.log("ERROR: Checking upvote achievements failed:");
