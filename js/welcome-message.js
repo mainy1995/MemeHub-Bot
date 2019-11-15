ceba271f1362a11425584ad7f525c2f558cc293f
LeifB <leif.bernsdorf@gmail.com>
2019-11-15 11:30:55 +0100

    fixed @undefined

--- js/welcome-message.js
+++ js/welcome-message.js
@@ -2,8 +2,13 @@ const config = require('../config/config.json');
 const util = require('util.js');
 
 async function greet_new_user(ctx) {
-   ctx.reply("Wilkommen @"+ctx.message.new_chat_member.username,
-     {reply_markup: {     
-  inline_keyboard:  [[{text:"Get Started",url:"https://www.t.me/mh_leif_bot?start="}]]}});     
+    ctx.reply(
+        config.public_welcome_message.replace("%USER%", util.name_from_user(ctx.message.new_chat_member)),
+        {
+            reply_markup: {
+                inline_keyboard: [[{ text: "Get Started", url: "https://www.t.me/mh_leif_bot?start=" }]]
+            }
+        }
+    );
 }
 module.exports.greet_new_user = greet_new_user;
