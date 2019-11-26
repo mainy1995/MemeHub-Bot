11ebdc9d563d6d61430dfe174c851b71f17b13be
LeifB <leif.bernsdorf@gmail.com>
2019-11-26 23:26:19 +0100

    Global error handling, graceful shutdown, bot name id

--- js/bot.js
+++ js/bot.js
@@ -1,14 +1,18 @@
 const Telegraf = require('telegraf');
 const _config = require('./config');
 const log = require('./log');
+const dockerNames = require('docker-names');
+const ShutdownHandler = require("node-shutdown-events");
+new ShutdownHandler({ exitTimeout: 50000 });
 
 const subscribers = [];
 let bot;
+let bot_name;
 let has_bot = false;
 _config.subscribe('config', async config => {
     if (has_bot) {
         has_bot = false;
-        log.warning('Stopping bot', 'Config has changed');
+        log.warning(`Stopping bot ${bot_name}`, 'Config has changed');
         bot.stop(() => start_bot(config));
         return;
     }
@@ -18,15 +22,17 @@ _config.subscribe('config', async config => {
 
 function start_bot(config) {
     bot = new Telegraf(config.bot_token);
+    bot_name = dockerNames.getRandomName();
+    bot.catch(handle_error);
     log.set_bot(bot);
     log.set_config(_config);
 
     bot.launch().then(() => {
-        log.success('Bot launched');
+        log.success(`Bot launched: ${bot_name}`);
         has_bot = true;
         notify_subscribers(bot);
     }, (err) => {
-        log.error("Cannot launch bot", err);
+        log.error(`Cannot launch bot: ${bot_name}`, err);
     });
 }
 
@@ -40,5 +46,17 @@ function notify_subscribers(bot) {
         callback(bot);
     }
 }
+
+function handle_error(error, context) {
+    log.error("Uncaught error", { error, context});
+}
+
+process.on("shutdown", async () => {
+    if (!has_bot) return;
+
+    log.warning(`Stopping bot ${bot_name}`, 'Shutdown event received');
+    return bot.stop();
+});
+
 
 module.exports.subscribe = subscribe;
